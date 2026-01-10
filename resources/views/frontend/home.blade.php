9ee3f81f62ae175adc8094269b1e4c9b4bebff3b
ibrahim <ibrahimkhalif5@gmail.com>
2026-01-10 22:10:37 +0300

    ('MM')

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -50,7 +50,7 @@
           </div>
 
           <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-            <img src="{{asset('assets/img/slider/el12.jpg')}}" class="img-fluid" alt="">
+            <img src="{{asset('assets/img/slider/MM.jpeg')}}" class="img-fluid" alt="">
           </div>
 
         </div>
